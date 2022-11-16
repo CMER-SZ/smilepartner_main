df42d032f25c9688527a75dc04e0550ad3e9f5c4
弗兰克 <1085068573@qq.com>
2022-11-16 16:36:07 +0800

    修改gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
       script: [
         {
           async: "async",
-          src:'https://www.googletagmanager.com/gtag/js?id=G-2G80LM1R8F'
+          src:'https://www.googletagmanager.com/gtag/js?id=G-B4J9QK7BQ1'
         },
         {
           innerHTML: `
@@ -44,10 +44,28 @@ export default defineNuxtConfig({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-2G80LM1R8F');
+          gtag('config', 'G-B4J9QK7BQ1');
           `,
           type: 'text/javascript',
         },
+        // {
+        //   innerHTML: `
+        //   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59MMG6F"
+        //   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        //   `,
+        //   type: 'text/javascript',
+        //   body:true
+        // },
+        // {
+        //   innerHTML: `
+        //   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        //   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        //   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        //   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        //   })(window,document,'script','dataLayer','GTM-59MMG6F');
+        //   `,
+        //   type: 'text/javascript',
+        // },
       ]
     },
   },
